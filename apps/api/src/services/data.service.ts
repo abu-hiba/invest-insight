5439da2658b755b79467415d69f9af8fc1cef766
Tom Bolton <tbolton96@icloud.com>
2020-05-13 23:11:07 +0100

    Api endpoint for company news

--- apps/api/src/services/data.service.ts
+++ apps/api/src/services/data.service.ts
@@ -1,11 +1,16 @@
 import iexApi from "../infrastructure/iex/iexApi"
+import { Company, NewsItem } from "../interfaces"
 
 export class DataService {
     public getCompanyProfile = async (symbol: string) => (
-        await iexApi(`/stock/${symbol}/company`) // TODO: Add promise type
+        await iexApi<Company>(`/stock/${symbol}/company`)
     ) 
 
     public searchCompanies = async (query: string) => (
-        await iexApi(`/search/${query}`)
+        await iexApi<Company[]>(`/search/${query}`)
+    )
+    
+    public companyNews = async (symbol: string, last: number) => (
+        await iexApi<NewsItem[]>(`/stock/${symbol}/news/last/${last}`)
     )
 }

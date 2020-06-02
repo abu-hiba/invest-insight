eda1ff52ef212bf8cbf3a9b5a32085b74e4600c7
Tom Bolton <tbolton96@icloud.com>
2020-06-02 10:06:21 +0100

    Error check when retrieving sector names

--- apps/app/src/components/MarketSectors.tsx
+++ apps/app/src/components/MarketSectors.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import { Container, Segment } from 'semantic-ui-react'
 import { useMarketRefData } from '../containers/IexContainer'
 
 const MarketSectors = () => {
     const { sectors, loading, error } = useMarketRefData()
     return (
-        <>
+        <Container>
             {loading
                 ? 'Loading...'
-                : sectors?.map(sector => 
-                    <p key={sector.name}>{sector.name}</p>
+                : (error
+                    ? 'Error loading market sectors'
+                    : sectors?.map(sector =>
+                        <Segment key={sector.name}>
+                            <h4>{sector.name}</h4>
+                        </Segment>
+                    )
                 )
             }
-        </>
+        </Container>
     )
 }
 

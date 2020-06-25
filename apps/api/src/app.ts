b5db18cbe318f98fef131a319602bf6afea5bc95
Tom Bolton <tbolton96@icloud.com>
2020-06-25 22:25:05 +0100

    Changed App to Server in api startup log statement

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -56,7 +56,7 @@ class App {
     
     public listen = () => {
         this.app.listen(process.env.PORT, () => {
-            console.log(`App listening on port ${process.env.PORT}`)
+            console.log(`Server listening on port ${process.env.PORT}`)
         })
     }
 }

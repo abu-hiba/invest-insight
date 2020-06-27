2deffc1ba58e21e8cb4b2fa005eddf88d2076645
Tom Bolton <tbolton96@icloud.com>
2020-06-27 16:51:22 +0100

    Class created for streaming iex stock data

--- apps/api/src/infrastructure/iex/iexApi.ts
+++ apps/api/src/infrastructure/iex/iexApi.ts
@@ -1,4 +1,26 @@
+import { Response } from 'express'
 import fetch from 'node-fetch'
+import https from 'https'
+
+export class IexSse {
+    constructor(private token: string, private res: Response) { }
+
+    public openStream = (symbols: string[]) => {
+        https.get({
+            agent: false,
+            path: `/stable/stocksUSNoUTP1Minute?token=${this.token}&symbols=${symbols.join()}`,
+            hostname: process.env.IEX_SSE
+        }, res => {
+            res.on('data', data => {
+                this.res.write(`${data.toString()}\n\n`)
+            })
+
+            res.on('error', err => {
+                console.log(err)
+            })
+        })
+    }
+}
 
 const iexApi = async <T>(path: string): Promise<T> => (
     await fetch(`${process.env.IEX_URL}${path}${path.includes('?') ? '&' : '?'}token=${process.env.IEX_TOKEN}`).then((res) =>

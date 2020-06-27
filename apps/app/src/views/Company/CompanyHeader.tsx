25a823c5c3cae307f83124784d7015d47daece35
Tom Bolton <tbolton96@icloud.com>
2020-06-27 16:59:06 +0100

    CompanyHeader moved to seperate component from CompanyProfile

---
+++ apps/app/src/views/Company/CompanyHeader.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import CSS from 'csstype'
+import { Placeholder, Header, Image, Button } from 'semantic-ui-react'
+import { CompanyState } from '../../containers/IexContainer'
+import { Quote } from '../../interfaces'
+import { round2Dp } from '../../utils/number'
+
+const CompanyHeaderText: CSS.Properties = {
+    marginLeft: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+}
+
+const CompanyHeaderImage: CSS.Properties = {
+    flexShrink: 0,
+    borderRadius: '5px',
+    height: 'auto:'
+}
+
+interface CompanyHeaderProps {
+    company: CompanyState,
+    event: Quote,
+    style?: CSS.Properties
+}
+
+const CompanyHeader: React.FC<CompanyHeaderProps> = ({ company, event, style }) => {
+    const { data, loading, error } = company
+    const { symbol, companyName, industry } = { ...data }
+    const { latestPrice, change, changePercent } = { ...event }
+
+    return (
+        <div style={{ display: 'flex', ...style }}>
+            {loading ? (
+                <>
+                    <Placeholder>
+                        <Placeholder.Image />
+                        <Placeholder.Header as='h2'/>
+                        <Placeholder.Header as='h3'/>
+                        <Placeholder.Header as='h4'/>
+                    </Placeholder>
+                </>
+            ) : error ? <>{error.message}</> : (
+                <>
+                    <Image
+                        src={`${process.env.LOGO_URL}/${data?.symbol}.png`}
+                        size='tiny'
+                        style={CompanyHeaderImage}
+                    />
+                    <div style={CompanyHeaderText}>
+                        <Header as='h3' style={{ margin: '0 0 10px 0' }}>
+                            {symbol}{' '}|{' '}
+                            <span style={{ color: '#545454' }}>{companyName}</span>
+                        </Header>
+                        <Header sub style={{ margin: '0 0 10px 0' }}>{industry}</Header>
+                        <p style={{ marginBottom: 0 }}>
+                            <span style={{ fontSize: '18px', fontWeight: 'bold' }}>
+                                {latestPrice}{' '}
+                            </span>
+                            <span style={{ color: change >= 0 ? 'green' : 'red', fontSize: '16px' }}>
+                                {change >= 0 && '+'}{change}{' '}
+                                ({round2Dp(changePercent * 100)}%)
+                            </span>
+                        </p>
+                        <p style={{ fontSize: '11px', color: '#545454' }}>
+                            Prices provided by <a href="https://iexcloud.io" target="_blank" rel="noopener noreferrer">IEX Cloud</a>
+                        </p>
+                    </div>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default CompanyHeader

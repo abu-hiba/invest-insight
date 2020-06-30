6c6a0d4b00157773635bfee1fb6ca3fc5b24e475
Tom Bolton <tbolton96@icloud.com>
2020-06-30 22:09:33 +0100

    useAuth hook called from Home view index and user data passed down

--- apps/app/src/views/Home/index.tsx
+++ apps/app/src/views/Home/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Watchlist from './Watchlist'
+import { useAuth } from '../../containers/AuthContext'
 
-const Home = () => (
-    <>
-        <Watchlist style={{ margin: '1em' }} />
-    </>
-)
+const Home = () => {
+    const { user } = useAuth()
+    return (
+        <>
+            <Watchlist user={user} style={{ margin: '1em' }} />
+        </>
+    )
+}
 
 export default Home

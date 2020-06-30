25afe375193182d4279162b28a8046b6a2b29a4c
Tom Bolton <tbolton96@icloud.com>
2020-06-30 22:03:18 +0100

    Placeholder for while price is loading

--- apps/app/src/components/Price.tsx
+++ apps/app/src/components/Price.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import CSS from 'csstype'
+import { Loader } from 'semantic-ui-react'
 import { round2Dp } from '../utils/number'
 
 const green = (color: string | undefined) => color ? 'rgb(0, 255, 0)' : 'green'
 
 interface PriceProps {
-    price: number,
-    change: number,
-    changePercent: number,
+    price: number | undefined,
+    change: number | undefined,
+    changePercent: number | undefined,
     style?: CSS.Properties,
     color?: string
 }
@@ -21,11 +22,11 @@ export const PriceAttribution = () => (
 const Price: React.FC<PriceProps> = ({ price, change, changePercent, style, color }) => (
     <p style={{ marginBottom: 0, ...style }}>
         <span style={{ fontSize: '18px', fontWeight: 'bold', color: color ? color : '#000' }}>
-            {price}{' '}
+            {price ? price : '--'}{' '}
         </span>
-        <span style={{ color: change >= 0 ? green(color) : 'rgb(255,0,0)', fontSize: '16px' }}>
-            {change >= 0 && '+'}{change}{' '}
-            ({round2Dp(changePercent * 100)}%)
+        <span style={{ color: change && change >= 0 ? green(color) : 'rgb(255,0,0)', fontSize: '16px' }}>
+            {change && change >= 0 && '+'}{change ? change : '--'}{' '}
+            ({changePercent ? round2Dp(changePercent * 100) : '--'}%)
         </span>
     </p>
 )

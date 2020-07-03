46f9e82a9e195c17ba92acfee59b702282ab965a
Tom Bolton <tbolton96@icloud.com>
2020-07-03 18:02:18 +0100

    Company NewsItem uses DarkSegment

--- apps/app/src/views/Company/CompanyNews.tsx
+++ apps/app/src/views/Company/CompanyNews.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, Fragment } from 'react'
 import { Card, Placeholder, Header } from 'semantic-ui-react'
+import DarkSegment from '../../components/Layout/DarkSegment'
 
 export interface CompanyNewsItemProps {
     loading: boolean,
@@ -19,27 +20,24 @@ export const CompanyNewsItem: React.FC<CompanyNewsItemProps> = ({
     headline,
     source,
     summary,
-    url 
-}) => (
-    loading ? (
-        <Placeholder
-            as={Card}
-            fluid
-        />
-    ) : (
-        <Card href={url} target="_blank" fluid>
-            <Card.Content>
-                <Card.Header as='h5'>{headline}</Card.Header>
-                <Card.Meta>{source}</Card.Meta>
-                <Card.Description>{summary}</Card.Description>
-            </Card.Content>
-        </Card>
-    )
-)
+    url,
+    lang
+}) => loading ? (
+    <Placeholder
+        as={Card}
+        fluid
+    />
+) : lang === 'en' ? (
+    <DarkSegment href={url}>
+        <h4 style={{ color: '#FFF', margin: '0 0 0.5rem 0' }}>{headline}</h4>
+        <h5 style={{ color: '#999', margin: '0 0 1rem 0' }}>{source}</h5>
+        <p style={{ color: '#FFF' }}>{summary}</p>
+    </DarkSegment>
+) : null
 
 export const NewsItems: React.FC<{ children: ReactNode }> = ({ children }) => (
     <div style={{ margin: '2em 1em' }}>
-        <Header as ='h3'>News</Header>
+        <Header as ='h3' style={{ color: '#FFF' }}>News</Header>
         <Card.Group>
             {React.Children.map(children, (child, i) =>
                 <Fragment key={i}>{child}</Fragment>

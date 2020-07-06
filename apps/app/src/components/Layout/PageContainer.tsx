8a0645d3d552cb03988d4f034a2b20489aa8265e
Tom Bolton <tbolton96@icloud.com>
2020-07-06 11:08:38 +0100

    PageContainer uses container interface

--- apps/app/src/components/Layout/PageContainer.tsx
+++ apps/app/src/components/Layout/PageContainer.tsx
@@ -1,20 +1,16 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import CSS from 'csstype'
+import { BasicContainer } from '../../interfaces'
 
-interface PageContainerProps {
-    children: ReactNode,
-    style?: CSS.Properties
-}
-
-const pageContainerStyle: CSS.Properties = {
+const pageContainer: CSS.Properties = {
     padding: '2em 1em 1em 1em',
     borderRadius: '10px',
     backgroundColor: '#1b1c1d',
     margin: '1em'
 }
 
-const PageContainer: React.FC<PageContainerProps> = ({ children, style }) => (
-    <div style={{ ...pageContainerStyle, ...style }}>
+const PageContainer: React.FC<BasicContainer> = ({ children, style }) => (
+    <div style={{ ...pageContainer, ...style }}>
         {children}
     </div>
 )

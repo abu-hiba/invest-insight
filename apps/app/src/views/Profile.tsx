5cfbd0f1c816f9a96019523075598ecd51811993
Tom Bolton <tbolton96@icloud.com>
2020-07-06 12:07:03 +0100

    Updated profile info page layout

--- apps/app/src/views/Profile.tsx
+++ apps/app/src/views/Profile.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 import { useAuth } from '../containers/AuthContext'
+import PageContainer from '../components/Layout/PageContainer'
 
 const Profile = () => {
     const { user } = useAuth()
+
+    const date = new Date(user.userData?.dateCreated!)
+
     return user.userData ? (
-        <>
-            <p>Username: {user.userData.username}</p>
-            <p>Email: {user.userData.email}</p>
-        </>
+        <PageContainer style={{ color: '#FFF' }}>
+            <h3>Username</h3>
+            <p>{user.userData.username}</p>
+            <hr style={{ borderColor: '#FFF' }} />
+            <h3>Email</h3>
+            <p>{user.userData.email}</p>
+            <hr style={{ borderColor: '#FFF' }} />
+            <h3>Account Created</h3>
+            <p>{date.toDateString()}</p>
+        </PageContainer>
     ) : (
         <Redirect to={{ pathname: '/' }} />
     )

2b07cb30652a9c8199d918ff249ca8f0ef6c6939
Tom Bolton <tbolton96@icloud.com>
2020-07-11 00:49:12 +0100

    MarketSectors component turned into more generic MarketCategory component to be used for both sectors and exchanges on Markets page

--- apps/app/src/views/Markets/MarketCategory.tsx
+++ apps/app/src/views/Markets/MarketCategory.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Responsive, Header } from 'semantic-ui-react'
 import CSS from 'csstype'
-import { useMarketRefData } from '../../containers/IexContainer'
 import SectionContainer from '../../components/Layout/SectionContainer'
 import DarkSegment from '../../components/Layout/DarkSegment'
 import SegmentContainer from '../../components/Layout/SegmentContainer'
+import { Sector, Exchange } from '../../interfaces'
+import { MarketCategory } from '../../containers/IexContainer'
 
-interface SectorSegmentProps {
+interface CategorySegmentProps {
     name: string,
     style?: CSS.Properties
 }
 
-const SectorSegment: React.FC<SectorSegmentProps> = ({ name, style }) => (
+const CategorySegment: React.FC<CategorySegmentProps> = ({ name, style }) => (
     <DarkSegment
         linkTo={`/sector/${name}`}
         style={{ display: 'flex', alignItems: 'center', ...style }}
@@ -22,27 +23,32 @@ const SectorSegment: React.FC<SectorSegmentProps> = ({ name, style }) => (
     </DarkSegment>
 )
 
-const MarketSectors: React.FC = () => {
-    const { sectors, loading, error } = useMarketRefData()
+interface CategoryProps {
+    category: MarketCategory<Sector[] | Exchange[]> | undefined,
+    header: string
+}
+
+const Category: React.FC<CategoryProps> = ({ category, header }) => {
+    const { data, loading, error } = { ...category }
     return (
         <SectionContainer>
-            <Header as='h2' style={{ color: '#FFF' }} dividing>Market Sectors</Header>
+            <Header as='h2' style={{ color: '#FFF' }} dividing>{header}</Header>
             <SegmentContainer>
-                {loading
-                    ? 'Loading...'
+                {loading || !category
+                    ? <span style={{ color: '#FFF' }}>Loading...</span>
                     : (error
                         ? 'Error loading market sectors'
-                        : sectors?.map(({ name }) =>
-                            <React.Fragment key={name}>
+                        : data?.map(({ name, description }: { name: string, description: string }) =>
+                            <React.Fragment key={name || description}>
                                 <Responsive
-                                    as={SectorSegment}
-                                    name={name}
+                                    as={CategorySegment}
+                                    name={name || description}
                                     style={{ margin: '0.3em', flexGrow: 1, textAlign: 'center' }}
                                     minWidth={500}
                                 />
                                 <Responsive
-                                    as={SectorSegment}
-                                    name={name}
+                                    as={CategorySegment}
+                                    name={name || description}
                                     style={{ margin: '0.2em', width: '48%'  }}
                                     maxWidth={500}
                                 />
@@ -55,4 +61,4 @@ const MarketSectors: React.FC = () => {
     )
 }
 
-export default MarketSectors
+export default Category
--- apps/app/src/views/Markets/index.tsx
+++ apps/app/src/views/Markets/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import MarketSectors from './MarketSectors'
+import React, { useEffect } from 'react'
+import Category from './MarketCategory'
+import { useMarketCategories } from '../../containers/IexContainer'
 
-const Markets = () => (
-    <>
-        <MarketSectors />
-    </>
-)
+const Markets = () => {
+    const { sectors, exchanges, fetchSectors, fetchExchanges } = useMarketCategories()
+
+    useEffect(() => {
+        fetchSectors()
+        fetchExchanges()
+    }, [])
+
+    return (
+        <>
+            <Category category={sectors} header='Sectors' />
+            <Category category={exchanges} header='Exchanges' />
+        </>
+    )
+}
 
 export default Markets

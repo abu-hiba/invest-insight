975699d506949dc8a78a4e9a283707f6a8ae0bbe
Tom Bolton <tbolton96@icloud.com>
2020-07-15 18:45:47 +0100

    Load more button added to Sector and Exchange pages to load more available symbols

--- apps/app/src/views/Sector/index.tsx
+++ apps/app/src/views/Sector/index.tsx
@@ -1,50 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Header, Responsive } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
 import { useSector } from '../../containers/IexContainer'
 import BreadcrumbBar from '../../components/Nav/BreadcrumbBar'
 import SectionContainer from '../../components/Layout/SectionContainer'
-import SegmentContainer from '../../components/Layout/SegmentContainer'
-import CompanySegment from '../../components/Layout/CompanySegment'
+import ExpandableCompanyList from '../../components/Layout/ExpandableCompanyList'
 
 const SectorPage: React.FC = () => {
     const { name } = useParams()
-    const { sectorData: { quotes, loading, error } } = useSector(name!)
+    const { sectorData } = useSector(name!)
 
     const sections = [
         { key: 'Markets', content: 'Markets', linkTo: '/markets' },
         { key: name, content: name, active: true }
     ]
-     
+
     return (
         <SectionContainer>
             {name && <BreadcrumbBar sections={sections} />}
             <Header as='h2' style={{ color: '#fff' }}>{name}</Header>
-            <SegmentContainer>
-                {!loading ? (
-                    error ? error.message : (
-                        quotes?.map((quote, i) =>
-                            i < 50 && 
-                                <React.Fragment key={quote.symbol}>
-                                    <Responsive
-                                        as={CompanySegment}
-                                        name={name}
-                                        style={{ margin: '0.3em', flexGrow: 1, width: '250px' }}
-                                        minWidth={500}
-                                        {...quote}
-                                    />
-                                    <Responsive
-                                        as={CompanySegment}
-                                        name={name}
-                                        style={{ margin: '0.2em', width: '100%' }}
-                                        maxWidth={500}
-                                        {...quote}
-                                    />
-                                </React.Fragment> 
-                        )
-                    )
-                ) : 'Loading'}
-                </SegmentContainer>
+            <ExpandableCompanyList {...sectorData} />
         </SectionContainer>
     )
 }
